f66c235924b4802842528c8547d962a6be2e42de
DevWonny <cjfdnjs1994@naver.com>
2022-10-05 13:42:01 +0900

    feat : page component 작성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Intro from "./view/Intro";
+import AboutMe from "./view/AboutMe";
+import Project from "./view/Project";
+import Contact from "./view/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Intro />
+      <AboutMe />
+      <Project />
+      <Contact />
+    </>
   );
 }
 
