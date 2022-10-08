132936b985203116d93b3175b47dd0a02e5edd8a
DevWonny <cjfdnjs1994@naver.com>
2022-10-08 14:24:48 +0900

    feat : Intro 문구 타이핑 효과 구현

--- src/view/Intro.jsx
+++ src/view/Intro.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 
@@ -6,10 +6,40 @@ import BackCommon from "../components/BackCommon";
 
 const Intro = () => {
   const location = useLocation();
+  const welcomeText = `Welcome! Cheolwon Portfolio!`;
+  // const welcomeNext = "";
+
+  // 문자열 하나씩 더해서 만들어 줄 state
+  const [firstTitle, setFirstTitle] = useState("");
+  // const [secondTitle, setSecondTitle] = useState("");
+
+  // 출력 될 문자 갯수 count state
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      setFirstTitle((prev) => {
+        let result = prev ? prev + welcomeText[count] : welcomeText[0];
+        setCount(count + 1);
+
+        if (count >= welcomeText.length) {
+          setCount(0);
+          setFirstTitle("");
+        }
+
+        return result;
+      });
+    }, 300);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  });
+
   return (
     <>
       <BackCommon pathname={location.pathname} />
-      <IntroWarp>Intro Text Location</IntroWarp>
+      <IntroWarp>{firstTitle}</IntroWarp>
     </>
   );
 };
@@ -22,7 +52,9 @@ const IntroWarp = styled.div`
   position: absolute;
   top: 250px;
   right: 5%;
-  border: 1px solid #000;
   text-align: center;
   line-height: 450px;
+  font-family: GmarketSansMedium;
+  font-size: 50px;
+  color: #a0bdd0;
 `;
