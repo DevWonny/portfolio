2993c6ce4489827f437c70c181ca25ad1bb2f551
DevWonny <cjfdnjs1994@naver.com>
2022-10-11 16:08:30 +0900

    feat : Project Page 구현 완료

--- src/view/Project.jsx
+++ src/view/Project.jsx
@@ -21,10 +21,27 @@ const Project = () => {
       <ProjectWrap>
         <ProjectContainer>
           {/* auton project */}
-          <ProjectItem>
+          <ProjectItem mobile={true}>
             <ProjectImage mobile={true}>
               <img src={autonImage} alt="auton_project" />
             </ProjectImage>
+            <ProjectContent>
+              <p>공임비교 Application</p>
+              <p>
+                &middot; 유저의 차량 정보를 통해 회원가입하여, 유저 위치에 따라
+                차량 정비소의 정보를 조회하고 예약하는 Application
+              </p>
+              <p>기간 : 2022.05 ~ 2022.08 (약 3개월)</p>
+              <p>
+                인원 : Back End - 2명 / Front End - 3명(1명 - ReactNative) /
+                Publisher - 1명 / Designer - 1명
+              </p>
+              <p>개발 언어 : JavaScript , React</p>
+              <p>
+                담당 역할 : 회원가입 및 차량등록, 수정 작업 / validation 작업 /
+                예약 페이지 작업 / 마이페이지 작업
+              </p>
+            </ProjectContent>
           </ProjectItem>
 
           {/* movie search project */}
@@ -32,15 +49,27 @@ const Project = () => {
             <ProjectImage>
               <img src={movieImage} alt="movieSearch_project" />
             </ProjectImage>
+            <ProjectContent>
+              <p>영화 검색 페이지</p>
+              <p>공공 API를 활용한 영화 검색 페이지 제작</p>
+              <p>기간 : 2022.08.03 ~ 2022.08.16</p>
+              <p>개발 언어 : JavaScript , React</p>
+            </ProjectContent>
           </ProjectItem>
         </ProjectContainer>
 
         <ProjectContainer>
           {/* covid19 project */}
-          <ProjectItem>
+          <ProjectItem mobile={true}>
             <ProjectImage mobile={true}>
               <img src={covidImage} alt="covid19_project" />
             </ProjectImage>
+            <ProjectContent>
+              <p>코로나 조회 Application</p>
+              <p>공공 API를 활용한 코로나 정보 조회 페이지 제작</p>
+              <p>기간 : 2022.08.17 ~ 2022.08.30</p>
+              <p>개발 언어 : JavaScript, React</p>
+            </ProjectContent>
           </ProjectItem>
 
           {/* clone coding project */}
@@ -48,6 +77,12 @@ const Project = () => {
             <ProjectImage>
               <img src={pizzaImage} alt="clone_project" />
             </ProjectImage>
+            <ProjectContent>
+              <p>클론코딩 - 피자선생 페이지</p>
+              <p>TypeScript와 MSW를 활용한 클론 코딩 작업</p>
+              <p>기간 : 2022.09.05 ~ 2022.09.26</p>
+              <p>개발 언어 : TypeScript, React</p>
+            </ProjectContent>
           </ProjectItem>
         </ProjectContainer>
       </ProjectWrap>
@@ -79,9 +114,8 @@ const ProjectWrap = styled.div`
 
 const ProjectContainer = styled.div`
   width: 100%;
-  height: 400px;
+  /* height: 400px; */
   margin-bottom: 50px;
-  background: red;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -89,15 +123,17 @@ const ProjectContainer = styled.div`
 
 const ProjectItem = styled.div`
   width: 45%;
-  height: 100%;
-  background: black;
-  color: #fff;
+  height: 500px;
+  position: relative;
+  display: flex;
+  flex-direction: ${(props) => (props.mobile ? "row" : "column")};
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ProjectImage = styled.div`
-  background-color: purple;
   width: ${(props) => (props.mobile ? "200px" : "100%")};
-  height: ${(props) => (props.mobile ? "100%" : "50%")};
+  height: ${(props) => (props.mobile ? "80%" : "50%")};
 
   & img {
     width: 100%;
@@ -105,4 +141,11 @@ const ProjectImage = styled.div`
   }
 `;
 
-const ProjectContent = styled.div``;
+const ProjectContent = styled.div`
+  width: 65%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: #a0bdd0;
+`;
