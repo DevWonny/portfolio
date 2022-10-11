4b17f78c23b629612acce0330598ffafbb465204
DevWonny <cjfdnjs1994@naver.com>
2022-10-11 14:22:46 +0900

    feat : intro 수정

--- src/view/Intro.jsx
+++ src/view/Intro.jsx
@@ -1,50 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import BackCommon from "../components/BackCommon";
 
 const Intro = () => {
   const location = useLocation();
-  const welcomeText = `Welcome! Cheolwon Portfolio!`;
-  // const welcomeNext = "";
-
-  // 문자열 하나씩 더해서 만들어 줄 state
-  const [firstTitle, setFirstTitle] = useState("");
-  // const [secondTitle, setSecondTitle] = useState("");
-
-  // 출력 될 문자 갯수 count state
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const typingInterval = setInterval(() => {
-      setFirstTitle((prev) => {
-        let result = prev ? prev + welcomeText[count] : welcomeText[0];
-        setCount(count + 1);
-
-        if (count >= welcomeText.length) {
-          setCount(0);
-          setFirstTitle("");
-        }
-
-        return result;
-      });
-    }, 300);
-
-    return () => {
-      clearInterval(typingInterval);
-    };
-  });
 
   return (
     <>
       <BackCommon pathname={location.pathname} />
-      <IntroWarp>{firstTitle}</IntroWarp>
+      <IntroWarp>
+        <div>
+          <span>W</span>
+          <span>e</span>
+          <span>l</span>
+          <span>c</span>
+          <span>o</span>
+          <span>m</span>
+          <span>e</span>
+          <span>!</span>
+        </div>
+
+        <p>Cheolwon Portfolio!</p>
+      </IntroWarp>
     </>
   );
 };
 
 export default Intro;
+
+const textUp = keyframes`
+  0%{
+    top : 0%;
+  }
+  20%{
+    top : -1rem;
+  }
+  40%{
+    top : 0;
+  }
+  60%{
+    top : 0;
+  }
+  80%{
+    top : 0;
+  }
+`;
+
+const textView = keyframes`
+  0%{
+    opacity: 0;
+  }
+  80%{
+    opacity: 1;
+  }
+`;
 
 const IntroWarp = styled.div`
   width: 55%;
@@ -52,9 +63,46 @@ const IntroWarp = styled.div`
   position: absolute;
   top: 250px;
   right: 5%;
-  text-align: center;
-  line-height: 450px;
   font-family: GmarketSansMedium;
   font-size: 50px;
   color: #a0bdd0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & span {
+    position: relative;
+    animation: ${textUp} 1.5s infinite;
+  }
+
+  & span:nth-of-type(1) {
+    animation-delay: 0.1s;
+  }
+  & span:nth-of-type(2) {
+    animation-delay: 0.2s;
+  }
+  & span:nth-of-type(3) {
+    animation-delay: 0.3s;
+  }
+  & span:nth-of-type(4) {
+    animation-delay: 0.4s;
+  }
+  & span:nth-of-type(5) {
+    animation-delay: 0.5s;
+  }
+  & span:nth-of-type(6) {
+    animation-delay: 0.6s;
+  }
+  & span:nth-of-type(7) {
+    animation-delay: 0.7s;
+  }
+  & span:nth-of-type(8) {
+    animation-delay: 0.8s;
+  }
+
+  & p {
+    margin: 0;
+    animation: ${textView} 3s;
+  }
 `;
