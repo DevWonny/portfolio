5686e48126c1aecc58c9f07860af2dc2295a95da
DevWonny <cjfdnjs1994@naver.com>
2022-10-11 15:41:59 +0900

    feat : Project page 이미지 구현

--- src/view/Project.jsx
+++ src/view/Project.jsx
@@ -3,23 +3,52 @@ import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import BackCommon from "../components/BackCommon";
 
+import autonImage from "../assets/auton_Intro.jpg";
+import movieImage from "../assets/movie_image.PNG";
+import covidImage from "../assets/covid19_image.PNG";
+import pizzaImage from "../assets/pizza_image.PNG";
+
 const Project = () => {
   const location = useLocation();
   // 각 Project Item 에는 이미지 및 제작 기간등을 입력하고,
   // Item 클릭 시 해당 Github 페이지로 이동되도록 설정
+
+  // auton ios app link = https://apps.apple.com/kr/app/%EA%B3%B5%EC%9E%84%EB%B9%84%EA%B5%90/id1631117693
   return (
     <>
       <BackCommon pathname={location.pathname} />
+
       <ProjectWrap>
         <ProjectContainer>
-          <ProjectItem>오토엔 프로젝트</ProjectItem>
-          <ProjectItem>모먼트 활용 프로젝트</ProjectItem>
-          <ProjectItem>영화 검색 프로젝트</ProjectItem>
+          {/* auton project */}
+          <ProjectItem>
+            <ProjectImage mobile={true}>
+              <img src={autonImage} alt="auton_project" />
+            </ProjectImage>
+          </ProjectItem>
+
+          {/* movie search project */}
+          <ProjectItem>
+            <ProjectImage>
+              <img src={movieImage} alt="movieSearch_project" />
+            </ProjectImage>
+          </ProjectItem>
         </ProjectContainer>
 
-        <ProjectContainer notThird={true}>
-          <ProjectItem>코로나 프로젝트</ProjectItem>
-          <ProjectItem>피자선생 클론코딩</ProjectItem>
+        <ProjectContainer>
+          {/* covid19 project */}
+          <ProjectItem>
+            <ProjectImage mobile={true}>
+              <img src={covidImage} alt="covid19_project" />
+            </ProjectImage>
+          </ProjectItem>
+
+          {/* clone coding project */}
+          <ProjectItem>
+            <ProjectImage>
+              <img src={pizzaImage} alt="clone_project" />
+            </ProjectImage>
+          </ProjectItem>
         </ProjectContainer>
       </ProjectWrap>
     </>
@@ -51,15 +80,29 @@ const ProjectWrap = styled.div`
 const ProjectContainer = styled.div`
   width: 100%;
   height: 400px;
+  margin-bottom: 50px;
+  background: red;
   display: flex;
-  justify-content: ${(props) =>
-    props.notThird ? "space-around" : "space-between"};
-  margin-bottom: 30px;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const ProjectItem = styled.div`
-  width: 30%;
+  width: 45%;
   height: 100%;
   background: black;
   color: #fff;
 `;
+
+const ProjectImage = styled.div`
+  background-color: purple;
+  width: ${(props) => (props.mobile ? "200px" : "100%")};
+  height: ${(props) => (props.mobile ? "100%" : "50%")};
+
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const ProjectContent = styled.div``;
