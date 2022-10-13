a7a6741080699fed95fa046f334fe61e316a53c6
DevWonny <cjfdnjs1994@naver.com>
2022-10-13 14:04:17 +0900

    feat : mobile header menu 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Header = () => {
+  // menu state
+  const [isMenu, setIsMenu] = useState(false);
+
   return (
     <HeaderWrap>
       <h1>Header Wrap</h1>
+      <Hamburger
+        onClick={() => {
+          setIsMenu(!isMenu);
+        }}
+      >
+        <HamburgerTop isMenu={isMenu} />
+        <HamburgreMiddle isMenu={isMenu} />
+        <HamburgerBottom isMenu={isMenu} />
+      </Hamburger>
     </HeaderWrap>
   );
 };
@@ -22,4 +34,47 @@ const HeaderWrap = styled.div`
   @media screen and (min-width: 1201px) {
     display: none;
   }
+`;
+
+const Hamburger = styled.div`
+  width: 24px;
+  height: 24px;
+  margin-left: 30px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HamburgerTop = styled.span`
+  position: absolute;
+  width: 24px;
+  height: 4px;
+  background: #d9d9d9;
+  border-radius: 3px;
+  transition: transform 0.2s, opacity 0.3s;
+  transform: ${(props) =>
+    props.isMenu ? "rotate(45deg)" : "translateY(-7px)"};
+`;
+
+const HamburgreMiddle = styled.span`
+  position: absolute;
+  width: 24px;
+  height: 4px;
+  background-color: #d9d9d9;
+  border-radius: 3px;
+  transition: transform 0.2s, opacity 0.3s;
+  transform: ${(props) => props.isMenu && "translateX(16px)"};
+  opacity: ${(props) => props.isMenu && "0"};
+`;
+
+const HamburgerBottom = styled.span`
+  position: absolute;
+  width: 24px;
+  height: 4px;
+  background-color: #d9d9d9;
+  border-radius: 3px;
+  transition: transform 0.2s, opacity 0.3s;
+  transform: ${(props) =>
+    props.isMenu ? "rotate(-45deg)" : "translateY(7px)"};
 `;
